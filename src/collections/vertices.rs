a5e7b6832b361e127d71690255074cebfeedc9e4
s-jad <s_jad@web.de>
2024-05-04 10:19:15 +0200

    Added: vertices for vertex shader

---
+++ src/collections/vertices.rs
@@ -0,0 +1,32 @@
+#[repr(C)]
+#[derive(Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+pub(crate) struct Vertex {
+    pub(crate) position: [f32; 2],
+}
+
+pub(crate) const VERTICES: &[Vertex; 6] = &[
+    // Bottom left triangle
+    Vertex {
+        position: [-1.0, -1.0],
+    },
+    Vertex {
+        position: [1.0, -1.0],
+    },
+    Vertex {
+        position: [-1.0, 1.0],
+    },
+    // Top right triangle
+    Vertex {
+        position: [1.0, -1.0],
+    },
+    Vertex {
+        position: [1.0, 1.0],
+    },
+    Vertex {
+        position: [-1.0, 1.0],
+    },
+];
+
+pub(crate) fn vertices_as_bytes(data: &[Vertex]) -> &[u8] {
+    bytemuck::cast_slice(data)
+}
